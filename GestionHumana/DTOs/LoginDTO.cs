a28b3f62876a80d82b6f28b0a123422317a49ed6
AlejoTorresLeon <103461707+AlejoTorresLeon@users.noreply.github.com>
2024-05-22 23:04:37 -0500

    Agregar archivos de proyecto.

---
+++ GestionHumana/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace GestionHumana.DTOs
+{
+    public class LoginDTO
+    {
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+    }
+}
---
+++ GestionHumana/Style/MyCustomTheme.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+
+namespace GestionHumana.Style
+{
+    // MyCustomTheme.cs
+    public class MyCustomTheme
+    {
+        public static MudTheme Theme = new MudTheme()
+        {
+            Palette = new PaletteLight()
+            {
+                Primary = "#386BC0",
+                AppbarBackground = "#386BC0"
+            }
+        };
+    }
+
+}
